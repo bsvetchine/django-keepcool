94511650d0805d4e9f5ca46f9d923d5a031dcd45
Bertrand Svetchine <bsvetchine@tandoori.pro>
2014-12-25 12:10:22 +0100

    keepcool | testers - _form.py upgrade.

--- keepcool/testers/_form.py
+++ keepcool/testers/_form.py
@@ -6,20 +6,12 @@ from ._base import BaseTester
 
 class FormTester(BaseTester):
 
-    """Mixin used in wizard view tests."""
-
     def test_form(self):
         for user in self.get_users():
+            self.log_user_in(user)
             for args in self.get_args(user):
                 url = reverse(self.url_name, args=args)
                 response = self.client.get(url)
                 self.assertEqual(response.status_code, 200)
                 response = self.client.post(url, self.form_data)
                 self.assertEqual(response.status_code, 200)
-
-    def go_though_form(self):
-        for user in self.get_users():
-            for args in self.get_args(user):
-                url = reverse(self.url_name, args=args)
-                response = self.client.post(url, self.form_data)
-                return self.client.get(response.url)
