51b4a3383d629e329a39154618be7d5f5337c918
Bertrand Svetchine <bsvetchine@tandoori.pro>
2014-12-25 12:55:36 +0100

    keepcool | testers - _form.py specified code from FormTester to CreateFormTester.

--- keepcool/testers/_form.py
+++ keepcool/testers/_form.py
@@ -4,14 +4,17 @@ from django.core.urlresolvers import reverse
 from ._base import BaseTester
 
 
-class FormTester(BaseTester):
+class CreateFormTester(BaseTester):
 
-    def test_form(self):
+    def test_create_form(self):
+        model = self.get_model_type(self.model).model_class()
         for user in self.get_users():
             self.log_user_in(user)
             for args in self.get_args(user):
+                initial_count = model.objects.count()
                 url = reverse(self.url_name, args=args)
                 response = self.client.get(url)
                 self.assertEqual(response.status_code, 200)
                 response = self.client.post(url, self.form_data)
                 self.assertEqual(response.status_code, 200)
+                self.assertEqual(model.objects.count(), initial_count+1)
