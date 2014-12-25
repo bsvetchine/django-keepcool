756deae597abe2c3d3b6ec2b57427f57cb486589
Bertrand Svetchine <bsvetchine@tandoori.pro>
2014-12-25 12:53:24 +0100

    keepcool | tests - __init__.py rename FormTester to CreateFormTester.

--- keepcool/testers/__init__.py
+++ keepcool/testers/__init__.py
@@ -1,10 +1,10 @@
-from ._form import FormTester
+from ._form import CreateFormTester
 from ._formwizard import FormwizardTester
 from ._view import ViewTester
 
 
 __all__ = [
-    "FormTester",
+    "CreateFormTester",
     "FormwizardTester",
     "ViewTester",
 ]
