6e29004424cec430f1bfac6659fadd74be643468
Bertrand Svetchine <bsvetchine@tandoori.pro>
2015-01-15 16:15:01 +0100

    keepcool | testers - added FormViewTester.

--- keepcool/testers/views/edit.py
+++ keepcool/testers/views/edit.py
@@ -6,55 +6,64 @@ from django.core.urlresolvers import reverse
 from .._base import BaseTester
 
 
+class FormViewTester(BaseTester):
+
+    """Generic Django FormView tester."""
+
+    def process(self, user=None):
+        for args in self._get_args(user=user):
+            url = reverse(self.url_name, args=args)
+            response = self.client.get(url)
+            while response.status_code in [301, 302]:
+                response = self.client.get(response.url)
+            self.assertEqual(response.status_code, 200)
+            response = self.client.post(url, self.form_data)
+            self.assertTrue(response.status_code in [200, 302])
+
+
 class CreateViewTester(BaseTester):
 
     """Generic Django CreateView tester."""
 
-    def test_view(self):
-        model = self.get_model_type(self.model).model_class()
-        for user in self.get_users():
-            self.log_user_in(user)
-            for args in self.get_args(user):
-                initial_count = model.objects.count()
-                url = reverse(self.url_name, args=args)
-                response = self.client.get(url)
-                self.assertEqual(response.status_code, 200)
-                response = self.client.post(url, self.form_data)
-                self.assertTrue(response.status_code in [200, 302])
-                self.assertEqual(model.objects.count(), initial_count+1)
+    def process(self, user=None):
+        model = self._get_model_type(self.model).model_class()
+        for args in self._get_args(user=user):
+            initial_count = model.objects.count()
+            url = reverse(self.url_name, args=args)
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 200)
+            response = self.client.post(url, self.form_data)
+            self.assertTrue(response.status_code in [200, 302])
+            self.assertEqual(model.objects.count(), initial_count+1)
 
 
 class UpdateViewTester(BaseTester):
 
     """Generic Django UpdateView tester."""
 
-    def test_view(self):
-        model = self.get_model_type(self.model).model_class()
-        for user in self.get_users():
-            self.log_user_in(user)
-            for args in self.get_args(user):
-                initial_count = model.objects.count()
-                url = reverse(self.url_name, args=args)
-                response = self.client.get(url)
-                self.assertEqual(response.status_code, 200)
-                response = self.client.post(url, self.form_data)
-                self.assertTrue(response.status_code in [200, 302])
-                self.assertEqual(model.objects.count(), initial_count)
+    def process(self, user=None):
+        model = self._get_model_type(self.model).model_class()
+        for args in self._get_args(user=user):
+            initial_count = model.objects.count()
+            url = reverse(self.url_name, args=args)
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 200)
+            response = self.client.post(url, self.form_data)
+            self.assertTrue(response.status_code in [200, 302])
+            self.assertEqual(model.objects.count(), initial_count)
 
 
 class DeleteViewTester(BaseTester):
 
     """Generic Django DeleteView tester."""
 
-    def test_view(self):
-        model = self.get_model_type(self.model).model_class()
-        for user in self.get_users():
-            self.log_user_in(user)
-            for args in self.get_args(user):
-                initial_count = model.objects.count()
-                url = reverse(self.url_name, args=args)
-                response = self.client.get(url)
-                self.assertEqual(response.status_code, 200)
-                response = self.client.post(url, self.form_data)
-                self.assertTrue(response.status_code in [200, 302])
-                self.assertEqual(model.objects.count(), initial_count-1)
+    def process(self, user=None):
+        model = self._get_model_type(self.model).model_class()
+        for args in self._get_args(user=user):
+            initial_count = model.objects.count()
+            url = reverse(self.url_name, args=args)
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 200)
+            response = self.client.post(url, self.form_data)
+            self.assertTrue(response.status_code in [200, 302])
+            self.assertEqual(model.objects.count(), initial_count-1)
